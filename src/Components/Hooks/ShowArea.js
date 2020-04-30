e2b41e631b763c99dc5b31c03bf19da8f0217157
menglijiao <15607005006@163.com>
2020-04-30 18:11:07 +0800

    Merge branch 'master' of https://github.com/autumnLeaves-lady/react-M
    # Conflicts:
    #	.idea/workspace.xml
    #	src/Components/LearnJS/index.js

--- src/Components/Hooks/ShowArea.js
+++ src/Components/Hooks/ShowArea.js
@@ -4,20 +4,18 @@ import {Button} from "antd";
 
 function ShowArea(props) {
     const {visible} = props;
-    const [one,setOne] = useState(0);
 
     useEffect(()=>{
         console.log('useEffect 1')
     });
-    console.log(useState(0))
     if(!visible) return null;
 
-    // useEffect(()=>{
-    //     console.log('useEffect 2')
-    // });
+    let res = useMemo(()=>{
+        return visible
+    });
     return (
         <div>
-            子组件
+            组件
         </div>
     )
 }
